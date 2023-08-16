f3e4553b0a90ebe94edb774f864546ae6ad8f3c8
Pratyush Poddar <pratyush.poddar@sequoia.com>
2023-08-16 14:47:05 +0530

    includes instead of indexOf

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const getJobStatus = (job) =>
     .then((res) => res.json())
     .then((data) =>
       data.filter(
-        (item) =>
-          item.name.indexOf('-PR-') === -1 && item.name.indexOf('-') !== -1,
+        (item) => !item.name.includes('-PR-') && item.name.includes('-'),
       ),
     );
 
