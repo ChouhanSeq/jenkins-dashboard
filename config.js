0924f0ff6b55786ae916a38f9e762625bbb9d2ca
Pratyush Poddar <pratyush.poddar@sequoia.com>
2023-09-06 13:45:26 +0530

    add fe prod

--- config.js
+++ config.js
@@ -30,5 +30,38 @@ module.exports = [
       },
     ],
   },
+  {
+    name: "FE Prod",
+    baseUrl: "https://jenkins-production.sequoia-development.com",
+    jobs: [
+      { name: "Adminshell", job: "pp-kernel-adminshell-frontend" },
+      {
+        name: "HRX",
+        job: "pp-sp-benefits-admincore-frontend",
+      },
+      {
+        name: "PX",
+        job: "pp-sp-benefits-px-frontend",
+      },
+      {
+        name: "TR",
+        job: "pp-ptr-totalrewards-frontend",
+      },
+      {
+        name: "Wellbeing",
+        job: "pp-sp-wellbeing-frontend",
+      },
+      {
+        name: "Kernel",
+        job: "pp-kernel-frontend",
+      },
+      {
+        name: "UWP",
+        job: "pp-po-s1-frontend",
+      },
+    ],
+  },
 ];
+
+
 
