0f99bc0bdd6ff322dcebea47e36b2f310bba9d16
Burhan <burhan.bharmal@sequoia.com>
2024-05-22 09:25:20 +0530

    update: shell, eureka & idm pipeline

--- config.js
+++ config.js
@@ -1,75 +1,68 @@
 const envs = [
   {
-    id: "qa",
-    name: "QA",
-    baseUrl: "https://jenkins-qa.sequoia-development.com",
+    id: 'qa',
+    name: 'QA',
+    baseUrl: 'https://jenkins-qa.sequoia-development.com',
   },
   {
-    id: "prod",
-    name: "Production",
-    baseUrl: "https://jenkins-production.sequoia-development.com",
+    id: 'prod',
+    name: 'Production',
+    baseUrl: 'https://jenkins-production.sequoia-development.com',
   },
 ];
 
 const dashboards = [
   {
-    id: "fe",
-    name: "Frontend",
+    id: 'fe',
+    name: 'Frontend',
     jobs: [
       {
-        name: "Adminshell",
-        job: "pp-kernel-adminshell-frontend",
-        env: "platform-frontend",
+        name: 'Adminshell',
+        job: 'kernel-adminshell-frontend',
+        env: 'platform-frontend',
       },
       {
-        name: "HRX",
-        job: "pp-sp-benefits-admincore-frontend",
-        env: "hrx-core-web-v2",
+        name: 'HRX',
+        job: 'pp-sp-benefits-admincore-frontend',
+        env: 'hrx-core-web-v2',
       },
       {
-        name: "PX",
-        job: "benefitos-benefits-frontend",
-        env: "benefitos-benefits-frontend",
+        name: 'PX',
+        job: 'benefitos-benefits-frontend',
+        env: 'benefitos-benefits-frontend',
       },
       {
-        name: "TR",
-        job: "compos-totalrewards-frontend",
-        env: "pp-totalrewards-frontend",
+        name: 'TR',
+        job: 'compos-totalrewards-frontend',
+        env: 'pp-totalrewards-frontend',
       },
       {
-        name: "Wellbeing",
-        job: "benefitos-wellbeing-frontend",
-        env: "benefitos-wellbeing-frontend",
+        name: 'Wellbeing',
+        job: 'benefitos-wellbeing-frontend',
+        env: 'benefitos-wellbeing-frontend',
       },
       {
-        name: "Kernel",
-        job: "pp-kernel-frontend",
-        env: "org-settings",
+        name: 'Kernel',
+        job: 'pp-kernel-frontend',
+        env: 'org-settings',
       },
       {
-        name: "UWP",
-        job: "serviceos-s1service-frontend",
-        env: "pp-po-uwp-frontend",
+        name: 'UWP',
+        job: 'serviceos-s1service-frontend',
+        env: 'pp-po-uwp-frontend',
       },
       {
-        name: "Eureka",
-        job: "pp-kernel-eureka-frontend",
-        env: "pp-kernel-eureka-frontend",
+        name: 'Eureka',
+        job: 'kernel-eureka-frontend',
+        env: 'pp-kernel-eureka-frontend',
       },
       {
-        name: "IDM",
-        job: "pp-kernel-idm-frontend",
-        env: "kernel_frontend",
+        name: 'IDM',
+        job: 'kernel-idm-frontend',
+        env: 'kernel_frontend',
       },
     ],
   },
 ];
 
 module.exports = { dashboards, envs };
-
-
-
-
-
-
-
